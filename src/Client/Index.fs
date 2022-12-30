332bc70b49bcf44586f426d8aae5b2d2d4f14f2b
Martin Bryant <mail@martinbryant.dev>
2022-12-30 16:47:14 +0000

    add page routing for Todo page

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,120 +1,41 @@
 module Index
 
 open Elmish
-open Fable.Remoting.Client
-open Shared
+open Feliz
+open Feliz.Router
 
-type Model = { Todos: Todo list; Input: string }
+type Page =
+    | TodoPage of TodoPage.Model
+
+type Model =
+    { CurrentPage: Page; CurrentUrl: string list }
 
 type Msg =
-    | GotTodos of Todo list
-    | SetInput of string
-    | AddTodo
-    | AddedTodo of Todo
-
-let todosApi =
-    Remoting.createApi ()
-    |> Remoting.withRouteBuilder Route.builder
-    |> Remoting.buildProxy<ITodosApi>
+    | UrlChanged of string list
+    | TodoPageMsg of TodoPage.Msg
 
 let init () : Model * Cmd<Msg> =
-    let model = { Todos = []; Input = "" }
+    let model, cmd = TodoPage.init ()
 
-    let cmd = Cmd.OfAsync.perform todosApi.getTodos () GotTodos
-
-    model, cmd
+    { CurrentPage = TodoPage model; CurrentUrl = Router.currentUrl() }, Cmd.map TodoPageMsg cmd
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
-    match msg with
-    | GotTodos todos -> { model with Todos = todos }, Cmd.none
-    | SetInput value -> { model with Input = value }, Cmd.none
-    | AddTodo ->
-        let todo = Todo.create model.Input
+    match model.CurrentPage, msg with
+    | _, UrlChanged url ->
+        { model with CurrentUrl = url }, Cmd.none
+    | TodoPage todoModel, TodoPageMsg todoMsg ->
+        let todoModel, todoCmd = TodoPage.update todoMsg todoModel
+        let nextState = { model with CurrentPage = TodoPage todoModel }
+        let nextCmd = Cmd.map TodoPageMsg todoCmd
+        nextState, nextCmd
 
-        let cmd = Cmd.OfAsync.perform todosApi.addTodo todo AddedTodo
-
-        { model with Input = "" }, cmd
-    | AddedTodo todo -> { model with Todos = model.Todos @ [ todo ] }, Cmd.none
-
-open Feliz
-open Feliz.Bulma
-
-let navBrand =
-    Bulma.navbarBrand.div [
-        Bulma.navbarItem.a [
-            prop.href "https://safe-stack.github.io/"
-            navbarItem.isActive
-            prop.children [
-                Html.img [
-                    prop.src "/favicon.png"
-                    prop.alt "Logo"
-                ]
-            ]
-        ]
-    ]
-
-let containerBox (model: Model) (dispatch: Msg -> unit) =
-    Bulma.box [
-        Bulma.content [
-            Html.ol [
-                for todo in model.Todos do
-                    Html.li [ prop.text todo.Description ]
-            ]
-        ]
-        Bulma.field.div [
-            field.isGrouped
-            prop.children [
-                Bulma.control.p [
-                    control.isExpanded
-                    prop.children [
-                        Bulma.input.text [
-                            prop.value model.Input
-                            prop.placeholder "What needs to be done now?"
-                            prop.onChange (fun x -> SetInput x |> dispatch)
-                        ]
-                    ]
-                ]
-                Bulma.control.p [
-                    Bulma.button.a [
-                        color.isPrimary
-                        prop.disabled (Todo.isValid model.Input |> not)
-                        prop.onClick (fun _ -> dispatch AddTodo)
-                        prop.text "Add"
-                    ]
-                ]
-            ]
-        ]
-    ]
+let viewPage (model: Model) (dispatch: Msg -> unit) =
+    match model.CurrentPage with
+    | TodoPage pageModel ->
+        TodoPage.view pageModel (dispatch << TodoPageMsg)
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Bulma.hero [
-        hero.isFullHeight
-        color.isPrimary
-        prop.style [
-            style.backgroundSize "cover"
-            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
-            style.backgroundPosition "no-repeat center center fixed"
-        ]
-        prop.children [
-            Bulma.heroHead [
-                Bulma.navbar [
-                    Bulma.container [ navBrand ]
-                ]
-            ]
-            Bulma.heroBody [
-                Bulma.container [
-                    Bulma.column [
-                        column.is6
-                        column.isOffset3
-                        prop.children [
-                            Bulma.title [
-                                text.hasTextCentered
-                                prop.text "todo-app"
-                            ]
-                            containerBox model dispatch
-                        ]
-                    ]
-                ]
-            ]
-        ]
+    React.router [
+        router.onUrlChanged (UrlChanged >> dispatch)
+        router.children [ viewPage model dispatch ]
     ]
