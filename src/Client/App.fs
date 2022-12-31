8e6a135161ebadb04fc4ecb63eaa611c1b6f4398
Martin Bryant <mail@martinbryant.dev>
2022-12-31 21:36:07 +0000

    switch to Elmish router as React package clashes with Feliz.Router

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -2,13 +2,30 @@ module App
 
 open Elmish
 open Elmish.React
-
+open Elmish.UrlParser
+open Elmish.Navigation
+open Urls
 #if DEBUG
 open Elmish.Debug
 open Elmish.HMR
 #endif
 
+let pageParser : Parser<Url->Url, Url> =
+  oneOf
+    [
+      map Url.Todo top
+      map Url.Todo (s Url.Todo.asString)
+    ]
+
+let urlUpdate (result:Option<Url>) model =
+  match result with
+  | Some url ->
+      Index.initFromUrl url
+  | None ->
+      model, Navigation.newUrl Url.NotFound.asString
+
 Program.mkProgram Index.init Index.update Index.view
+|> Program.toNavigable (parsePath pageParser) urlUpdate
 #if DEBUG
 |> Program.withConsoleTrace
 #endif
--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,41 +1,48 @@
 module Index
 
 open Elmish
+open Urls
 open Feliz
-open Feliz.Router
+open Feliz.Bulma
 
 type Page =
     | TodoPage of TodoPage.Model
+    | NotFound
 
 type Model =
-    { CurrentPage: Page; CurrentUrl: string list }
+    { CurrentPage: Page }
 
 type Msg =
-    | UrlChanged of string list
-    | TodoPageMsg of TodoPage.Msg
+    TodoPageMsg of TodoPage.Msg
 
-let init () : Model * Cmd<Msg> =
-    let model, cmd = TodoPage.init ()
+let initFromUrl url =
+    match url with
+    | Url.Todo ->
+        let model, cmd = TodoPage.init ()
+        { CurrentPage = TodoPage model }, Cmd.map TodoPageMsg cmd
+    | Url.NotFound -> { CurrentPage = NotFound }, Cmd.none
 
-    { CurrentPage = TodoPage model; CurrentUrl = Router.currentUrl() }, Cmd.map TodoPageMsg cmd
+let init (url: Url option): Model * Cmd<Msg> =
+    match url with
+    | Some url -> initFromUrl url
+    | _ -> { CurrentPage = NotFound }, Cmd.none
+
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match model.CurrentPage, msg with
-    | _, UrlChanged url ->
-        { model with CurrentUrl = url }, Cmd.none
     | TodoPage todoModel, TodoPageMsg todoMsg ->
         let todoModel, todoCmd = TodoPage.update todoMsg todoModel
         let nextState = { model with CurrentPage = TodoPage todoModel }
         let nextCmd = Cmd.map TodoPageMsg todoCmd
         nextState, nextCmd
+    | NotFound, _ ->
+        model, Cmd.none
 
 let viewPage (model: Model) (dispatch: Msg -> unit) =
     match model.CurrentPage with
     | TodoPage pageModel ->
         TodoPage.view pageModel (dispatch << TodoPageMsg)
+    | NotFound -> Html.h1 "Not found"
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    React.router [
-        router.onUrlChanged (UrlChanged >> dispatch)
-        router.children [ viewPage model dispatch ]
-    ]
+    viewPage model dispatch
