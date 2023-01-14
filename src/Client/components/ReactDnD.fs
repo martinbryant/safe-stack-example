51ac8588b2d505538cd7174a54cc300aedf74b0f
Martin Bryant <mail@martinbryant.dev>
2023-01-14 13:11:35 +0000

    progress - but component renders with errors

--- src/Client/components/ReactDnD.fs
+++ src/Client/components/ReactDnD.fs
@@ -4,13 +4,25 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fable.React
 
+type DraggableProvided = {
+    innerRef: Browser.Types.Element -> unit
+}
+
+type DraggableStateSnapshot = {
+    isDragging : bool
+}
+
+type DraggableRenderProps = (DraggableProvided * DraggableStateSnapshot) -> ReactElement
+
 type DraggableProps =
     | Index of int
     | DraggableId of obj
     | Key of obj
+    | Children of DraggableRenderProps
 
-let inline draggable (props: DraggableProps list) (elems: ReactElement list) : ReactElement =
-    ofImport "Draggable" "react-beautiful-dnd" (keyValueList CaseRules.LowerFirst props) elems
+let inline draggable (props: DraggableProps list) (render: DraggableRenderProps) : ReactElement =
+    let allProps = props @ [Children render]
+    ofImport "Draggable" "react-beautiful-dnd" (keyValueList CaseRules.LowerFirst allProps) []
 
 type DragDropContextProps =
     | OnDragEnd of (obj[] -> unit)
@@ -18,17 +30,20 @@ type DragDropContextProps =
 let inline dragDropContext (props: DragDropContextProps list) (elems: ReactElement list) : ReactElement =
     ofImport "DragDropContext" "react-beautiful-dnd" (keyValueList CaseRules.LowerFirst props) elems
 
-type DroppableProps =
-    | DroppableId of obj
+type DroppableProvided = {
+    innerRef: Browser.Types.Element -> unit
+}
 
 type DroppableStateSnapshot = {
     isDraggingOver : bool
 }
+type DroppableRenderProps = (DroppableProvided * DroppableStateSnapshot) -> ReactElement
 
-type DroppableProvided = {
-    innerRef : Browser.Types.Element -> unit
-}
+type DroppableProps =
+    | DroppableId of obj
+    | Children of DroppableRenderProps
 
-let inline droppable (props: DroppableProps list) (elem: (DroppableProps * DroppableStateSnapshot) -> ReactElement) : ReactElement =
-    let children = elem
-    ofImport "Droppable" "react-beautiful-dnd" (keyValueList CaseRules.LowerFirst props) [ elem ]
+
+let inline droppable (props: DroppableProps list) (render: DroppableRenderProps) : ReactElement =
+    let allProps = Seq.append props [Children render]
+    ofImport "Droppable" "react-beautiful-dnd" (keyValueList CaseRules.LowerFirst allProps) []
